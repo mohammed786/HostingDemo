39ad6abb5e3991042b25b38912bd37eea7c252af
Mohammed Modi <mohammed.modi@loginradius.com>
2019-08-04 17:02:09 +0530

    Updates

---
+++ precache-manifest.b72813233a62a575f29cee474790dbd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e3e215e3ce24128fba8bb581b17e525",
+    "url": "mohammeditraders.dns-cloud.net/index.html"
+  },
+  {
+    "revision": "fa79644e242d5ee7dd4e",
+    "url": "mohammeditraders.dns-cloud.net/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "mohammeditraders.dns-cloud.net/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fa79644e242d5ee7dd4e",
+    "url": "mohammeditraders.dns-cloud.net/static/js/main.e560a503.chunk.js"
+  },
+  {
+    "revision": "73da4cad9f906ab5fc22",
+    "url": "mohammeditraders.dns-cloud.net/static/js/runtime~main.680850ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "mohammeditraders.dns-cloud.net/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.52f9154079dc61e2df3b2bbe03cf97d3.js"
+  "mohammeditraders.dns-cloud.net/precache-manifest.b72813233a62a575f29cee474790dbd4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("mohammeditraders.dns-cloud.net/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
