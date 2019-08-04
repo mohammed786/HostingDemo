722f79ba24b3c43b9e49f8d2c5671648e2fffd0d
Mohammed Modi <mohammed.modi@loginradius.com>
2019-08-04 18:03:47 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "mohammeditraders.dns-cloud.net/precache-manifest.b72813233a62a575f29cee474790dbd4.js"
+  "/precache-manifest.52f9154079dc61e2df3b2bbe03cf97d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("mohammeditraders.dns-cloud.net/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
